5914d3bd060b0ed1f884eb44c90196822b3dfe15
Ian P. Cooke <ipc@informatic.io>
2020-08-31 16:05:20 -0500

    add tests and fixes for cancel and detach.

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use async_executor::Task as AsyncExecutorTask;
 #[cfg(feature = "tokio-compat")]
 use tokio::{self, task::JoinHandle as TokioJoinHandle};
 
-#[cfg(any(feature = "tokio-compat", feature="futures-compat"))]
+#[cfg(any(feature = "tokio-compat", feature = "futures-compat"))]
 use futures::future::{AbortHandle, Abortable};
 
 #[cfg(feature = "smol-compat")]
@@ -28,7 +28,7 @@ use once_cell::sync::OnceCell;
 #[cfg(feature = "futures-compat")]
 use futures::{executor::ThreadPool, future::RemoteHandle};
 
-#[cfg(any(feature="tokio-compat", feature="futures-compat"))]
+#[cfg(any(feature = "tokio-compat", feature = "futures-compat"))]
 use futures::FutureExt;
 
 #[derive(Error, Debug)]
@@ -146,16 +146,25 @@ impl<T: 'static> JoinHandle<T> {
             JoinHandle::None(_) => panic!("you can not detatch JoinHandle::None!"),
             #[cfg(feature = "tokio-compat")]
             JoinHandle::Tokio((handle, abort_handle)) => {
-                let maybe_output = handle.now_or_never();
-                let output = if let Some(Ok(output)) = maybe_output {
-                    Some(output)
-                } else {
-                    None
-                };
-                if let Some(abort_handle) = abort_handle {
-                    abort_handle.abort();
+                match abort_handle {
+                    Some(abort_handle) => {
+                        let maybe_output = handle.now_or_never();
+                        let output = if let Some(Ok(output)) = maybe_output {
+                            Some(output)
+                        } else {
+                            None
+                        };
+                        abort_handle.abort();
+                        output
+                    }
+                    None => {
+                        // cancel shouldn't return until the task is canceled or finished so we have to wait
+                        match handle.await {
+                            Ok(output) => Some(output),
+                            Err(_) => None,
+                        }
+                    }
                 }
-                output
             }
             #[cfg(feature = "smol-compat")]
             JoinHandle::Smol(task) => task.cancel().await,
@@ -165,11 +174,17 @@ impl<T: 'static> JoinHandle<T> {
             JoinHandle::AsyncExecutor(task) => task.cancel().await,
             #[cfg(feature = "futures-compat")]
             JoinHandle::Futures((handle, abort_handle)) => {
-                let output = handle.now_or_never();
-                if let Some(abort_handle) = abort_handle {
-                    abort_handle.abort();
+                match abort_handle {
+                    Some(abort_handle) => {
+                        let output = handle.now_or_never();
+                        abort_handle.abort();
+                        output
+                    }
+                    None => {
+                        // cancel shouldn't return until the task is canceled or finished so we have to wait
+                        Some(handle.await)
+                    }
                 }
-                output
             }
         }
     }
@@ -261,116 +276,252 @@ impl Spawner {
 mod tests {
     use crate::{Executor, JoinHandle, SpawnError, Spawner};
     use futures::executor::ThreadPool;
+    use std::time::Duration;
+
+    async fn detach_test() -> Result<(), Box<dyn std::error::Error>> {
+        let (tx, rx) = async_channel::unbounded();
+        let handle = Spawner::spawn(async move {
+            async_io::Timer::new(Duration::from_micros(500)).await;
+            tx.send(()).await.unwrap();
+        });
+
+        handle.detach();
+
+        match rx.recv().await {
+            Ok(_) => {} // pass
+            Err(_) => panic!("you should receive something because the task should run to completion!"),
+        };
+
+        Ok::<(), Box<dyn std::error::Error>>(())
+    }
+
+    async fn cancel_test_none() -> Result<(), Box<dyn std::error::Error>> {
+        let (tx, rx) = async_channel::unbounded();
+        let handle = Spawner::spawn(async move {
+            async_io::Timer::new(Duration::from_secs(5)).await;
+            tx.send(()).await.unwrap();
+        });
+
+        let output = handle.cancel().await;
+        assert_eq!(output, None);
+
+        match rx.recv().await {
+            Ok(_) => panic!("you should receive nothing because the task was canceled!"),
+            Err(_) => {} //pass
+        };
+
+        Ok::<(), Box<dyn std::error::Error>>(())
+    }
+
+    async fn cancel_test_some() -> Result<(), Box<dyn std::error::Error>> {
+        let handle = Spawner::spawn(async {
+            2u64 + 2u64
+        });
+
+        async_io::Timer::new(Duration::from_micros(100)).await; //give the spawned future time to run
+
+        let output = handle.cancel().await;
+        assert_eq!(output, Some(4));
+
+        Ok::<(), Box<dyn std::error::Error>>(())
+    }
 
     rusty_fork_test! {
-        #[test]
-        #[should_panic]
-        fn spawn_no_executor_set_fails() {
+    #[test]
+    #[should_panic]
+    fn spawn_no_executor_set_fails() {
+        let _ = Spawner::spawn(async { 2 + 2 });
+    }
+
+    #[test]
+    fn async_executor_spawn() {
+        Executor::AsyncExecutor.set().unwrap();
+        let ex = async_executor::Executor::new();
+        ex.run(async {
+            let handle = Spawner::spawn(async { 2 + 2 });
+            if let JoinHandle::AsyncExecutor(_) = handle {
+                //pass
+            } else {
+                panic!("wrong join handle!");
+            }
+            let output = handle.await?;
+            assert_eq!(output, 4);
+            Ok::<(), SpawnError>(())
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn tokio_spawn() {
+        Executor::Tokio.set().unwrap();
+        let mut ex = tokio::runtime::Runtime::new().unwrap();
+        ex.block_on(async {
+            let handle = Spawner::spawn(async { 2 + 2 });
+            if let JoinHandle::Tokio(_) = handle {
+                //pass
+            } else {
+                panic!("wrong join handle!");
+            }
+            let output = handle.await?;
+            assert_eq!(output, 4);
+            Ok::<(), SpawnError>(())
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn smol_spawn() {
+        Executor::Smol.set().unwrap();
+        smol::run(async {
+            let handle = Spawner::spawn(async { 2 + 2 });
+            if let JoinHandle::Smol(_) = handle {
+                //pass
+            } else {
+                panic!("wrong join handle!");
+            }
+            let output = handle.await?;
+            assert_eq!(output, 4);
+            Ok::<(), SpawnError>(())
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn async_std_spawn() {
+        Executor::AsyncStd.set().unwrap();
+        async_std::task::block_on(async {
+            let handle = Spawner::spawn(async { 2 + 2 });
+            if let JoinHandle::AsyncStd(_) = handle {
+                //pass
+            } else {
+                panic!("wrong join handle!");
+            }
+            let output = handle.await?;
+            assert_eq!(output, 4);
+            Ok::<(), SpawnError>(())
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn futures_spawn() {
+        let ex = ThreadPool::new().unwrap();
+        Executor::Futures(ex).set().unwrap();
+
+        futures::executor::block_on(async move {
+            let handle = Spawner::spawn(async { 2 + 2 });
+            if let JoinHandle::Futures(_) = handle {
+                //pass
+            } else {
+                panic!("wrong join handle!");
+            }
+            let output = handle.await?;
+            assert_eq!(output, 4);
+            Ok::<(), SpawnError>(())
+        })
+        .unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn tokio_cannot_spawn_async_executor() {
+        Executor::Tokio.set().unwrap();
+        let ex = async_executor::Executor::new();
+        ex.run(async {
             let _ = Spawner::spawn(async { 2 + 2 });
-        }
+        });
+    }
 
-        #[test]
-        fn async_executor_spawn()  {
-            Executor::AsyncExecutor.set().unwrap();
-            let ex = async_executor::Executor::new();
-            ex.run(async {
-                let handle = Spawner::spawn(async { 2 + 2 });
-                if let JoinHandle::AsyncExecutor(_) = handle {
-                    //pass
-                } else {
-                    panic!("wrong join handle!");
-                }
-                let output = handle.await?;
-                assert_eq!(output, 4);
-                Ok::<(), SpawnError>(())
-            }).unwrap();
-        }
+    #[test]
+    #[should_panic]
+    fn async_executor_cannot_spawn_tokio() {
+        Executor::AsyncExecutor.set().unwrap();
+        let mut ex = tokio::runtime::Runtime::new().unwrap();
+        ex.block_on(async {
+            let _ = Spawner::spawn(async { 2 + 2 });
+        });
+    }
 
-        #[test]
-        fn tokio_spawn() {
-            Executor::Tokio.set().unwrap();
-            let mut ex = tokio::runtime::Runtime::new().unwrap();
-            ex.block_on(async {
-                let handle = Spawner::spawn(async { 2 + 2 });
-                if let JoinHandle::Tokio(_) = handle {
-                    //pass
-                } else {
-                    panic!("wrong join handle!");
-                }
-                let output = handle.await?;
-                assert_eq!(output, 4);
-                Ok::<(), SpawnError>(())
-            }).unwrap();
-        }
+    #[test]
+    fn cancel_async_executor_none() {
+        Executor::AsyncExecutor.set().unwrap();
+        let ex = async_executor::Executor::new();
 
-        #[test]
-        fn smol_spawn() {
-            Executor::Smol.set().unwrap();
-            smol::run(async {
-                let handle = Spawner::spawn(async { 2 + 2 });
-                if let JoinHandle::Smol(_) = handle {
-                    //pass
-                } else {
-                    panic!("wrong join handle!");
-                }
-                let output = handle.await?;
-                assert_eq!(output, 4);
-                Ok::<(), SpawnError>(())
-            }).unwrap();
-        }
+        ex.run(cancel_test_none()).unwrap();
+    }
+    #[test]
+    fn cancel_async_executor_some() {
+        Executor::AsyncExecutor.set().unwrap();
+        let ex = async_executor::Executor::new();
+        ex.run(cancel_test_some()).unwrap();
+    }
 
-        #[test]
-        fn async_std_spawn() {
-            Executor::AsyncStd.set().unwrap();
-            async_std::task::block_on(async {
-                let handle = Spawner::spawn(async { 2 + 2 });
-                if let JoinHandle::AsyncStd(_) = handle {
-                    //pass
-                } else {
-                    panic!("wrong join handle!");
-                }
-                let output = handle.await?;
-                assert_eq!(output, 4);
-                Ok::<(), SpawnError>(())
-            }).unwrap();
-        }
+    #[test]
+    fn cancel_tokio_none() {
+        Executor::Tokio.set().unwrap();
+        let mut ex = tokio::runtime::Runtime::new().unwrap();
+        ex.block_on(cancel_test_none()).unwrap();
+    }
 
-        #[test]
-        fn futures_spawn() {
-            let ex = ThreadPool::new().unwrap();
-            Executor::Futures(ex).set().unwrap();
+    #[test]
+    fn cancel_tokio_executor_some() {
+        Executor::AsyncExecutor.set().unwrap();
+        let ex = async_executor::Executor::new();
+        ex.run(cancel_test_some()).unwrap();
+    }
 
-            futures::executor::block_on( async move {
-                let handle = Spawner::spawn(async { 2 + 2 });
-                if let JoinHandle::Futures(_) = handle {
-                    //pass
-                } else {
-                    panic!("wrong join handle!");
-                }
-                let output = handle.await?;
-                assert_eq!(output, 4);
-                Ok::<(), SpawnError>(())
-            }).unwrap();
-        }
+    #[test]
+    fn cancel_smol_none() {
+        Executor::Smol.set().unwrap();
+        smol::run(cancel_test_none()).unwrap();
+    }
 
-        #[test]
-        #[should_panic]
-        fn tokio_cannot_spawn_async_executor() {
-            Executor::Tokio.set().unwrap();
-            let ex = async_executor::Executor::new();
-            ex.run(async {
-                let _ = Spawner::spawn(async { 2 + 2 });
-            });
-        }
+    #[test]
+    fn cancel_smol_executor_some() {
+        Executor::Smol.set().unwrap();
+        smol::run(cancel_test_some()).unwrap();
+    }
 
-        #[test]
-        #[should_panic]
-        fn async_executor_cannot_spawn_tokio() {
-            Executor::AsyncExecutor.set().unwrap();
-            let mut ex = tokio::runtime::Runtime::new().unwrap();
-            ex.block_on(async {
-                let _ = Spawner::spawn(async { 2 + 2 });
-            });
-        }
+    #[test]
+    fn cancel_futures_none() {
+        let ex = ThreadPool::new().unwrap();
+        Executor::Futures(ex).set().unwrap();
+        futures::executor::block_on(cancel_test_none()).unwrap();
+    }
+
+    #[test]
+    fn cancel_futures_some() {
+        let ex = ThreadPool::new().unwrap();
+        Executor::Futures(ex).set().unwrap();
+        futures::executor::block_on(cancel_test_some()).unwrap();
+    }
+
+    #[test]
+    fn detach_async_executor() {
+        Executor::AsyncExecutor.set().unwrap();
+        let ex = async_executor::Executor::new();
+        ex.run(detach_test()).unwrap();
+    }
+
+    #[test]
+    fn detach_tokio() {
+        Executor::Tokio.set().unwrap();
+        let mut ex = tokio::runtime::Runtime::new().unwrap();
+        ex.block_on(detach_test()).unwrap();
+    }
+
+    #[test]
+    fn detach_smol() {
+        Executor::Smol.set().unwrap();
+        smol::run(detach_test()).unwrap();
+    }
+
+    #[test]
+    fn detach_futures() {
+        let ex = ThreadPool::new().unwrap();
+        Executor::Futures(ex).set().unwrap();
+        futures::executor::block_on(detach_test()).unwrap();
+    }
+
     }
 }
